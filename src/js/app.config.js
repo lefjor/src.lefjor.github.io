d6408592e2507c11519706e27dc99076c2864f46
lefjor <lefjor@users.noreply.github.com>
2017-04-11 15:19:59 +0200

    mode html 5 for url

--- src/js/app.config.js
+++ src/js/app.config.js
@@ -1,7 +1,8 @@
 'use strict';
 
-module.exports = ['$routeProvider', function ($routeProvider) {
-
+module.exports = ['$routeProvider', function ($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+    
     $routeProvider.
         when('/', {
             template: '<resume-component></resume-component>'
